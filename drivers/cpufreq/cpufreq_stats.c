8ba3acfca8387010139727503908786f86fb298c
Nirmal Abraham <nabrah@codeaurora.org>
2015-10-07 16:23:43 +0530

    cpufreq: Correct the data reported in all_time_in_state
    Commit bd9474e059bbb2bb62f7e93894cfc3d3ba473fb2 (cpufreq_stats:
    Adds the fucntionality to load current values for each frequency
    for all the cores) introduced a change by which
    'cpufreq_allstats_create' gets called at initialization (from
    'cpufreq_stats_init' instead of 'cpufreq_stat_notifier_policy').
    This causes 'cpufreq_allstate_create' to be called before the
    freq_table is allocated from 'create_all_freq_table'. Due to
    this, the data for cpu's which are online at boot are not
    added to the 'all_freq_table' leading to the incorrect
    reporting of data when the below sysfs command is run -
    'cat sys/devices/system/cpu/cpufreq/all_time_in_state'.
    
    Correct this behaviour by altering the cpufreq_stats init
    sequence by which the memory for 'all_freq_table' is allocated
    before the 'cpufreq_allstats_create' function is called.
    
    Change-Id: I2232dacdc0deec4d1987c418e833fe28f74623fc
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -551,6 +551,18 @@ static void add_bL_freq_table(unsigned int freq)
 	bL_freq_table->time_in_state[bL_freq_table->table_size++] = 0ULL;
 }
 
+static void free_all_freq_table(void)
+{
+	if (all_freq_table) {
+		if (all_freq_table->freq_table) {
+			kfree(all_freq_table->freq_table);
+			all_freq_table->freq_table = NULL;
+		}
+		kfree(all_freq_table);
+		all_freq_table = NULL;
+	}
+}
+
 static void add_all_freq_table(unsigned int freq)
 {
 	size_t size;
@@ -764,6 +776,8 @@ static int cpufreq_stats_setup(void)
 	if (ret)
 		return ret;
 
+	create_all_freq_table();
+
 	register_hotcpu_notifier(&cpufreq_stat_cpu_notifier);
 	for_each_online_cpu(cpu)
 		cpufreq_update_policy(cpu);
@@ -776,10 +790,10 @@ static int cpufreq_stats_setup(void)
 		unregister_hotcpu_notifier(&cpufreq_stat_cpu_notifier);
 		for_each_online_cpu(cpu)
 			cpufreq_stats_free_table(cpu);
+		free_all_freq_table();
 		return ret;
 	}
 
-	create_all_freq_table();
 	ret = sysfs_create_file(cpufreq_global_kobject,
 			&_attr_all_time_in_state.attr);
 	if (ret)
