a29212337a7c0389cbe82611bab00c6e54b14ed8
Ruchi Kandoi <kandoiruchi@google.com>
2016-12-10 19:07:22 +0000

    sched: cpufreq: Adds a field cpu_power in the task_struct
    cpu_power has been added to keep track of amount of power each task is
    consuming. cpu_power is updated whenever stime and utime are updated for
    a task. power is computed by taking into account the frequency at which
    the current core was running and the current for cpu actively
    running at hat frequency.
    
    Change-Id: Ic535941e7b339aab5cae9081a34049daeb44b248
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    
    Original commit: https://github.com/dianlujitao/CAF_kernel_msm-3.10/commit/85a6bd2bc4c903df43186e6f41209746aa6fdf05

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -23,6 +23,7 @@
 #include <linux/sort.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#include <linux/sched.h>
 #include <asm/cputime.h>
 #ifdef CONFIG_BL_SWITCHER
 #include <asm/bL_switcher.h>
@@ -177,6 +178,24 @@ static ssize_t show_bL_all_time_in_state(struct kobject *kobj,
 			(unsigned long long) cputime64_to_clock_t(bL_freq_table->time_in_state[i]));
 	}
 }
+
+void acct_update_power(struct task_struct *task, cputime_t cputime) {
+	struct cpufreq_power_stats *powerstats;
+	struct cpufreq_stats *stats;
+	unsigned int cpu_num, curr;
+
+	if (!task)
+		return;
+	cpu_num = task_cpu(task);
+	powerstats = per_cpu(cpufreq_power_stats, cpu_num);
+	stats = per_cpu(cpufreq_stats_table, cpu_num);
+	if (!powerstats || !stats)
+		return;
+
+	curr = powerstats->curr[stats->last_index];
+	task->cpu_power += curr * cputime_to_usecs(cputime);
+}
+EXPORT_SYMBOL_GPL(acct_update_power);
 
 static ssize_t show_current_in_state(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
