1cbe30a2946342d9332dc77305f5c248fe5d00b8
Lunarixus <Nathanb644@hotmail.co.uk>
2020-06-05 01:48:26 +0000

    drivers/thermal: Drop duplicate definitions
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- drivers/staging/samsung/sec_slow.c
+++ drivers/staging/samsung/sec_slow.c
@@ -120,7 +120,7 @@ static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
 	return cputime_to_usecs(idle_time);
 }
 
-static u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
+u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
 
